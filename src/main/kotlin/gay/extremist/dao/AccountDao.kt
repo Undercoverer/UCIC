e158ca9fc50d321c1a577ada7352f78ab4b27f5a
Dallin Pierce <dspierce@uncg.edu>
2024-03-14 18:50:29 -0400

    started adding CRUD for accounts

--- src/main/kotlin/gay/extremist/dao/AccountDao.kt
+++ src/main/kotlin/gay/extremist/dao/AccountDao.kt
@@ -6,4 +6,6 @@ interface AccountDao {
 
     suspend fun allAccounts(): List<Account>
     suspend fun addNewAccount(username : String, email : String, password : String, token : String)
+    suspend fun deleteAccount(accountID : Int)
+    suspend fun getToken(email : String)
 }
