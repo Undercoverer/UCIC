85d261356f64bcbcf84d82c1dfdd8f388ef17794
Dallin Pierce <dspierce@uncg.edu>
2024-03-14 23:49:10 -0400

    CRUD for account added

--- src/main/kotlin/gay/extremist/dao/AccountDaoImpl.kt
+++ src/main/kotlin/gay/extremist/dao/AccountDaoImpl.kt
@@ -1,29 +1,13 @@
 package gay.extremist.dao
 
-import gay.extremist.models.Account
-import gay.extremist.models.Accounts
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.insert
+import gay.extremist.dao.DatabaseFactory.dbQuery
+import gay.extremist.models.*
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+
 
 class AccountDaoImpl : AccountDao{
-
-    override suspend fun allAccounts(): List<Account> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun addNewAccount(username: String, email: String, password: String, token: String) {
-        val insertStatement = Accounts.insert {
-            it[Accounts.username] = username
-            it[Accounts.email] = email
-            it[Accounts.password] = password
-            it[Accounts.token] = token
-        }
-        insertStatement.resultedValues?.singleOrNull()?.let(::resultRowToAccount)
-    }
-
-    override suspend fun deleteAccount(accountID: Int) {
-        TODO("Not yet implemented")
-    }
 
     private fun resultRowToAccount(row : ResultRow) = Account(
         accountID = row[Accounts.accountID],
@@ -34,4 +18,51 @@ class AccountDaoImpl : AccountDao{
 
     )
 
+    override suspend fun allAccounts(): List<Account> = dbQuery{
+        Accounts
+            .selectAll()
+            .map(::resultRowToAccount)
+    }
+
+    override suspend fun account(accountID: Int): Account? = dbQuery{
+        Accounts
+            .select {Accounts.accountID eq accountID}
+            .map(::resultRowToAccount)
+            .singleOrNull()
+    }
+
+    override suspend fun addNewAccount(username: String, email: String, password: String, token: String): Account? = dbQuery {
+        val insertStatement = Accounts.insert {
+            it[Accounts.username] = username
+            it[Accounts.email] = email
+            it[Accounts.password] = password
+            it[Accounts.token] = token
+        }
+        insertStatement.resultedValues?.singleOrNull()?.let(::resultRowToAccount)
+    }
+
+    override suspend fun editAccount(accountID: Int, username: String, email: String, password: String): Boolean = dbQuery {
+        Accounts.update({ Accounts.accountID eq accountID }) {
+            it[Accounts.username] = username
+            it[Accounts.email] = email
+            it[Accounts.password] = password
+        } > 0
+    }
+
+    override suspend fun deleteAccount(accountID: Int): Boolean = dbQuery {
+        Accounts.deleteWhere { Accounts.accountID eq accountID } > 0
+    }
+
+    override suspend fun getToken(email: String) {
+        TODO("Not yet implemented")
+    }
+
+}
+
+val dao: AccountDao = AccountDaoImpl().apply {
+    runBlocking {
+        if(allAccounts().isEmpty()) {
+            addNewAccount("admin","admin@fakemail.com","password","token" )
+        }
+    }
 }
