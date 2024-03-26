db98f9b68869ca9e05a2743790a09fc5a5827c9a
Fox Johnjulio <foxkj01@gmail.com>
2024-03-26 13:42:17 -0400

    feat: Refactor comment routes to separate functions
    - Extracted comment route logic into separate functions
    - Improve readability and maintainability of the code  ;)))

--- src/main/kotlin/gay/extremist/plugins/routes/comment.kt
+++ src/main/kotlin/gay/extremist/plugins/routes/comment.kt
@@ -10,178 +10,85 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.pipeline.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Route.createCommentRoutes() = route("/comments") {
-    val commentDao = commentDao
-    // Comments for a video
     route("/video/{id}") {
         // Get
-        get {
-            val videoId = call.parameters["id"]
-            videoId ?: return@get call.respond(ErrorResponse.videoIdNotProvided)
-
-            runCatching {
-                videoId.toInt()
-            }.onFailure {
-                return@get call.respond(ErrorResponse.videoNonNumericId)
-            }
-            call.respond(commentDao.getCommentsOnVideo(videoId.toInt()).map {
-                transaction { CommentModel(it.video.id.value, it.account.id.value, it.id.value, it.comment) }
-            })
-        }
+        get { handleGetComments() }
         // Create
-        post {
-            val videoId = call.parameters["id"]
-            videoId ?: return@post call.respond(ErrorResponse.videoIdNotProvided)
-
-            runCatching {
-                videoId.toInt()
-            }.onFailure {
-                return@post call.respond(ErrorResponse.videoNonNumericId)
-            }
-
-            val video = videoDao.readVideo(videoId.toInt())
-            video ?: return@post call.respond(ErrorResponse.videoNotFound)
-
-
-            val accountId = call.request.headers["accountId"]
-            accountId ?: return@post call.respond(ErrorResponse.accountIdNotProvided)
-
-            runCatching {
-                accountId.toInt()
-            }.onFailure {
-                return@post call.respond(ErrorResponse.accountNonNumericId)
-            }
-
-            val account = accountDao.readAccount(accountId.toInt())
-            account ?: return@post call.respond(ErrorResponse.accountNotFound)
-
-            val token = call.request.headers["token"]
-            token ?: return@post call.respond(ErrorResponse.accountTokenNotProvided)
-
-            if (account.token != token) {
-                return@post call.respond(ErrorResponse.accountTokenInvalid)
-            }
-
-            var commentText = ""
-            runCatching {
-                commentText = call.receive<String>()
-            }.onFailure {
-                return@post call.respond(ErrorResponse.commentNotProvided)
-            }
-
-            val parentCommentId = call.request.headers["commentId"]
-            val parentComment = parentCommentId?.let {
-                runCatching {
-                    parentCommentId.toInt()
-                }.onFailure {
-                    return@post call.respond(ErrorResponse.commentNonNumericId)
-                }
-                val parentComment = commentDao.readComment(parentCommentId.toInt())
-                parentComment ?: return@post call.respond(ErrorResponse.commentNotFound)
-            }
-
-
-            val comment = commentDao.createComment(
-                account, video, parentComment, commentText
-            )
-
-            call.respond(status = HttpStatusCode.Created, message = "Comment created")
-
-        }
+        post { handleCreateComment() }
         // Update
-        post {
-            val commentId = call.request.headers["commentId"]
-            commentId ?: return@post call.respond(ErrorResponse.commentIdNotProvided)
-
-            runCatching {
-                commentId.toInt()
-            }.onFailure {
-                return@post call.respond(ErrorResponse.commentNonNumericId)
-            }
-
-            val comment = commentDao.readComment(commentId.toInt())
-            comment ?: return@post call.respond(ErrorResponse.commentNotFound)
-
-            val accountId = call.request.headers["accountId"]
-            accountId ?: return@post call.respond(ErrorResponse.accountIdNotProvided)
-
-            runCatching {
-                accountId.toInt()
-            }.onFailure {
-                return@post call.respond(ErrorResponse.accountNonNumericId)
-            }
-
-            val account = accountDao.readAccount(accountId.toInt())
-            account ?: return@post call.respond(ErrorResponse.accountNotFound)
-
-            if (account.id.value != comment.account.id.value) {
-                return@post call.respond(ErrorResponse.accountNotCommentAuthor)
-            }
-            val token = call.request.headers["token"]
-            token ?: return@post call.respond(ErrorResponse.accountTokenNotProvided)
-            if (account.token != token) {
-                return@post call.respond(ErrorResponse.accountTokenInvalid)
-            }
-
-            var commentText = ""
-            runCatching {
-                commentText = call.receive<String>()
-                commentDao.updateComment(commentId.toInt(), commentText)
-                call.respond(status = HttpStatusCode.Accepted, message = "Comment updated")
-            }.onFailure {
-                return@post call.respond(ErrorResponse.commentNotProvided)
-            }
-        }
-        delete {
-            val commentId = call.request.headers["commentId"]
-            commentId ?: return@delete call.respond(ErrorResponse.commentIdNotProvided)
-            runCatching {
-                commentId.toInt()
-            }.onFailure {
-                return@delete call.respond(ErrorResponse.commentNonNumericId)
-            }
-
-            val comment = commentDao.readComment(commentId.toInt())
-            comment ?: return@delete call.respond(ErrorResponse.commentNotFound)
-            val accountId = call.request.headers["accountId"]
-            accountId ?: return@delete call.respond(ErrorResponse.accountIdNotProvided)
-
-            runCatching {
-                accountId.toInt()
-            }.onFailure {
-                return@delete call.respond(ErrorResponse.accountNonNumericId)
-            }
-
-            val account = accountDao.readAccount(accountId.toInt())
-            account ?: return@delete call.respond(ErrorResponse.accountNotFound)
-
-            if (account.id.value != comment.account.id.value) {
-                return@delete call.respond(ErrorResponse.accountNotCommentAuthor)
-            }
-
-            val token = call.request.headers["token"]
-            token ?: return@delete call.respond(ErrorResponse.accountTokenNotProvided)
-
-            if (account.token != token) {
-                return@delete call.respond(ErrorResponse.accountTokenInvalid)
-            }
-            commentDao.deleteComment(commentId.toInt())
-            call.respond(status = HttpStatusCode.Accepted, message = "Comment deleted")
-        }
+        put { handleUpdateComment() }
+        delete { handleDeleteComment() }
     }
     // Comments for a user
-    get("/account/{id}") {
-        val accountId = call.parameters["id"]
-        accountId ?: return@get call.respond(ErrorResponse.accountIdNotProvided)
+    get("/account/{id}") { handleGetCommentsForAccount() }
+}
 
-        runCatching {
-            accountId.toInt()
-        }.onFailure {
-            return@get call.respond(ErrorResponse.accountNonNumericId)
-        }
 
-        call.respond(commentDao.getCommentsOnAccount(accountId.toInt()))
+suspend fun PipelineContext<Unit, ApplicationCall>.handleGetComments() = with(call) {
+    val videoId = parameters["id"]?.toIntOrNull() ?: return respond(ErrorResponse.videoIdNotProvided)
+    respond(commentDao.getCommentsOnVideo(videoId).map {
+        transaction { CommentModel(it.video.id.value, it.account.id.value, it.id.value, it.comment) }
+    })
+}
+
+suspend fun PipelineContext<Unit, ApplicationCall>.handleCreateComment() {
+    with(call) {
+        val videoId = parameters["id"]?.toIntOrNull() ?: return respond(ErrorResponse.videoIdNotProvided)
+        val video = videoDao.readVideo(videoId) ?: return respond(ErrorResponse.videoNotFound)
+        val accountId =
+            request.headers["accountId"]?.toIntOrNull() ?: return respond(ErrorResponse.accountIdNotProvided)
+        val account = accountDao.readAccount(accountId) ?: return respond(ErrorResponse.accountNotFound)
+        val token = request.headers["token"] ?: return respond(ErrorResponse.accountTokenNotProvided)
+        if (account.token != token) return respond(ErrorResponse.accountTokenInvalid)
+        val commentText = receive<String>()
+        val parentCommentId = request.headers["commentId"]?.toIntOrNull()
+        val parentComment =
+            parentCommentId?.let { commentDao.readComment(it) } ?: return respond(ErrorResponse.commentNotFound)
+        commentDao.createComment(account, video, parentComment, commentText)
+        respond(status = HttpStatusCode.Created, message = "Comment created")
+    }
+}
+
+suspend fun PipelineContext<Unit, ApplicationCall>.handleUpdateComment() {
+    with(call) {
+        val commentId =
+            request.headers["commentId"]?.toIntOrNull() ?: return respond(ErrorResponse.commentIdNotProvided)
+        val comment = commentDao.readComment(commentId) ?: return respond(ErrorResponse.commentNotFound)
+        val accountId =
+            request.headers["accountId"]?.toIntOrNull() ?: return respond(ErrorResponse.accountIdNotProvided)
+        val account = accountDao.readAccount(accountId) ?: return respond(ErrorResponse.accountNotFound)
+        if (account.id.value != comment.account.id.value) return respond(ErrorResponse.accountNotCommentAuthor)
+        val token = request.headers["token"] ?: return respond(ErrorResponse.accountTokenNotProvided)
+        if (account.token != token) return respond(ErrorResponse.accountTokenInvalid)
+        val commentText = receive<String>()
+        commentDao.updateComment(commentId, commentText)
+        respond(status = HttpStatusCode.Accepted, message = "Comment updated")
+    }
+}
+
+suspend fun PipelineContext<Unit, ApplicationCall>.handleDeleteComment() {
+    with(call) {
+        val commentId =
+            request.headers["commentId"]?.toIntOrNull() ?: return respond(ErrorResponse.commentIdNotProvided)
+        val comment = commentDao.readComment(commentId) ?: return respond(ErrorResponse.commentNotFound)
+        val accountId =
+            request.headers["accountId"]?.toIntOrNull() ?: return respond(ErrorResponse.accountIdNotProvided)
+        val account = accountDao.readAccount(accountId) ?: return respond(ErrorResponse.accountNotFound)
+        if (account.id.value != comment.account.id.value) return respond(ErrorResponse.accountNotCommentAuthor)
+        val token = request.headers["token"] ?: return respond(ErrorResponse.accountTokenNotProvided)
+        if (account.token != token) return respond(ErrorResponse.accountTokenInvalid)
+        commentDao.deleteComment(commentId)
+        respond(status = HttpStatusCode.Accepted, message = "Comment deleted")
+    }
+}
+
+suspend fun PipelineContext<Unit, ApplicationCall>.handleGetCommentsForAccount() {
+    with(call) {
+        val accountId = parameters["id"]?.toIntOrNull() ?: return respond(ErrorResponse.accountIdNotProvided)
+        respond(commentDao.getCommentsOnAccount(accountId))
     }
 }
