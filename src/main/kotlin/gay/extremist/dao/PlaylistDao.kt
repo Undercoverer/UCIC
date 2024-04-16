fc5394e018dd6831ea33db10b5976fa0925aea74
Dallin Pierce <dspierce@uncg.edu>
2024-04-15 22:51:09 -0400

    modified playlists to remove descriptions modified some account and tag endpoints to better meet frontend needs

--- src/main/kotlin/gay/extremist/dao/PlaylistDao.kt
+++ src/main/kotlin/gay/extremist/dao/PlaylistDao.kt
@@ -5,10 +5,10 @@ import gay.extremist.models.Playlist
 import gay.extremist.models.Video
 
 interface PlaylistDao {
-    suspend fun createPlaylist(owner: Account, name: String, description: String): Playlist
+    suspend fun createPlaylist(owner: Account, name: String): Playlist
     suspend fun readPlaylist(id: Int): Playlist?
     suspend fun readPlaylistAll(): List<Playlist>
-    suspend fun updatePlaylist(id: Int, name: String, description: String): Boolean
+    suspend fun updatePlaylist(id: Int, name: String): Boolean
     suspend fun deletePlaylist(id: Int): Boolean
     suspend fun addVideoToPlaylist(id: Int, video: Video): Boolean
     suspend fun removeVideoFromPlaylist(id: Int, video: Video): Boolean
